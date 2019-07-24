1e83cdf9233be9422434434e4b3056d9de2913b1
saji021198 <52283311+saji021198@users.noreply.github.com>
2019-07-24 11:32:14 -0700

    Update palin.py

--- palin.py
+++ palin.py
@@ -3,9 +3,16 @@ k=len(d)
 c=round(k/2)
 string1=""
 rev=d[k-1:c-1:-1]
-for i in range (1,c):
+rev2=d[ : :-1]
+if(k%2!=0):
+   for i in range (1,c):
     string1=string1+d[i]
-if(string1==rev):
-    print("yes")
+    if(string1==rev):
+        print("yes")
+    else:
+        print("no")
 else:
-    print("no")
+    if(d==rev2):
+        print("yes")
+    else:
+        print("no")
